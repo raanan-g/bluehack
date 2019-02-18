a589bc01648d9347ae5223485c35ed746d7692b5
raanan-g <41341275+raanan-g@users.noreply.github.com>
2019-02-18 09:48:57 -0500

    Update mapbox.js

--- mapbox.js
+++ mapbox.js
@@ -4,12 +4,52 @@ container: 'map',
 style: 'mapbox://styles/raanan-g/cjs3vdafw04bb1fmtnqdivns0',
 center: [-79.990100, 40.441700],
 zoom: 10.0
+});
 
+var marker = new mapboxgl.Marker() // Initialize a new marker
+  .setLngLat([-79.990100, 40.441700])
+  .addTo(map); // Ass the marker to the map
 
+var geocoder = new MapboxGeocoder({ // Initialize the geocoder
+  accessToken: mapboxgl.accessToken, // Set the access token
+  placeholder: 'Search your address', // Text prompt
+  bbox: [, , ,], // Boundary for Pittsburgh
+  proximity: {
+    longitude: -79.990100,
+    latitude: -40.441700
+  } // coordinates of layer centroid
+});
 
+// Add geocoder to the map
+map.addControl(geocoder);
 
+// Add a source layer and default styling for a single point
+map.on('load', function() {
+  map.addSource('single-point', {
+    "type": "geojson",
+    "data": {
+      "type": "FeatureCollection",
+      "features": []
+    }
+  });
+  
+  map.addLayer({
+    "id":"point",
+    "source":"single-point",
+    "type": "circle",
+    "paint": {
+      "circle-radius": 10,
+      "circle-color": "448ee4"
+    }
+  });
 
+  // When user searches and address,  
+  // add a marker at the result's coordinates
+  geocoder.on('result', function(e) {
+    map.GetSource('single-point').setData(e.result.geometry);
 
+  });
+  
 });
 
 /* var layers = ['0-17%', '17-33%', '33-50%', '50-67%', '67-83%', '83-100%'];
